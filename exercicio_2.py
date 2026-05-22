2d08aa3621c3c0ef5f3c99d596e41d819e05e8d2
higor2100 <higor2100@gmail.com>
2026-05-22 20:02:05 -0300

    Feitos 1 e 2

--- exercicio_2.py
+++ exercicio_2.py
@@ -25,3 +25,8 @@ Pode-se utilizar o comando "while" ou até o "for" para te ajudar nisso.
 
 Fonte: Curso em vídeo.
 """
+numero = int(input("Digite um número inteiro:\n"))
+fatorial = numero
+for i in range(numero-1, 1,-1):
+    fatorial *= i
+print(f"O fatorial de {numero} é {fatorial}")
